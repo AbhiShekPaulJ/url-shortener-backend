bd8f2ac6b9b3c6c07a892780748a3a5cde852456
Abhishek Paul <jabhishekpaul079@gmail.com>
2025-07-21 17:19:43 +0530

    updated to work globally

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,21 @@
 import express from "express"
 import { getAllUserUrls } from "../controller/user.controller.js"
 import { authMiddleware } from "../middleware/auth.middleware.js"
+import UrlModel from "../models/url.model.js";
 
 const router = express.Router()
 
 router.post("/urls",authMiddleware, getAllUserUrls)
+
+router.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const urlEntry = await UrlModel.findOne({ short_url: shortId });
+
+  if (urlEntry) {
+    return res.redirect(urlEntry.original_url);
+  } else {
+    return res.status(404).json({ message: "URL not found" });
+  }
+});
 
 export default router
