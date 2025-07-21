e6bb1b02ad37c6f5c93c7dce292dfd42cf09f7d4
Abhishek Paul <jabhishekpaul079@gmail.com>
2025-07-21 14:14:41 +0530

    Added .gitignore and backend files

---
+++ src/controller/auth.controller.js
@@ -0,0 +1,42 @@
+import { cookieOptions } from "../config/config.js"
+import { loginUser, registerUser } from "../services/auth.service.js"
+import wrapAsync from "../utils/tryCatchWrapper.js"
+
+
+
+export const register_user = wrapAsync( async (req, res) => {
+    const {name, email, password} = req.body
+    const {token,user} = await registerUser(name, email, password)
+    req.user = user
+    res.cookie("accessToken", token, cookieOptions)
+    res.status(200).json({message:"register success"})
+})
+
+export const login_user = wrapAsync( async (req, res) => {
+    const {email, password} = req.body
+    const {token,user} = await loginUser(email, password)
+
+    req.user = user
+    res.cookie("accessToken", token, cookieOptions)
+    res.status(200).json({user:user,message:"login success"})
+})
+
+export const logout_user = wrapAsync( async (req, res) => {
+    res.clearCookie("accessToken", cookieOptions)
+    res.status(200).json({message:"logout success"})
+})
+
+export const get_current_user = wrapAsync(async (req, res) => {
+    console.log("req.user", req.user);
+    if (!req.user) {
+      return res.status(401).json({ authenticated: false });
+    }
+  
+    res.status(200).json({
+      authenticated: true,
+      user: {
+        name: req.user.name,
+        email: req.user.email,
+      }
+    });
+  });
