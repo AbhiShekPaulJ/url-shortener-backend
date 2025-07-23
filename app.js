3573ce81c3f88d305d950636bac89de5cfdac26f
Abhishek Paul <jabhishekpaul079@gmail.com>
2025-07-23 13:40:28 +0530

    just in case update if any left out

--- app.js
+++ app.js
@@ -1,44 +1,50 @@
 import express from "express";
-import {nanoid} from "nanoid"
-import dotenv from "dotenv"
-import connectDB from "./src/config/monogo.config.js"
-import short_url from "./src/routes/short_url.route.js"
-import user_routes from "./src/routes/user.routes.js"
-import auth_routes from "./src/routes/auth.routes.js"
+import { nanoid } from "nanoid";
+import dotenv from "dotenv";
+import connectDB from "./src/config/monogo.config.js";
+import short_url from "./src/routes/short_url.route.js";
+import user_routes from "./src/routes/user.routes.js";
+import auth_routes from "./src/routes/auth.routes.js";
 import { redirectFromShortUrl } from "./src/controller/short_url.controller.js";
 import { errorHandler } from "./src/utils/errorHandler.js";
-import cors from "cors"
+import cors from "cors";
 import { attachUser } from "./src/utils/attachUser.js";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
-app.use(cors({
-    origin: ['https://url-shortener-frontend-csnq.onrender.com',
-        'http://localhost:5174',
-        'https://shorturl-zkubzg1uz-abhishekpauljs-projects.vercel.app',
-        'https://shorturl-seven-henna.vercel.app'
+app.use(
+  cors({
+    origin: [
+      "https://url-shortener-frontend-csnq.onrender.com",
+      "http://localhost:5174",
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "http://localhost:5174",
+      "http://127.0.0.1:5174",
+      "https://shorturl-zkubzg1uz-abhishekpauljs-projects.vercel.app",
+      "https://shorturl-seven-henna.vercel.app",
     ],
-    credentials: true
-  }));
+    credentials: true,
+  })
+);
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use(attachUser)
+app.use(attachUser);
 
-app.use("/api/user",user_routes)
-app.use("/api/auth",auth_routes)
-app.use("/api/create",short_url)
-app.get("/:id",redirectFromShortUrl)
+app.use("/api/user", user_routes);
+app.use("/api/auth", auth_routes);
+app.use("/api/create", short_url);
+app.get("/:id", redirectFromShortUrl);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(3000,()=>{
-    connectDB()
-    console.log("Server is running on port 3000");
-})
-
+app.listen(3000, () => {
+  connectDB();
+  console.log("Server is running on port 3000");
+});
