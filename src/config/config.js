4ae4e5fb6035b2f202f2a747d76ca365b953c92e
Abhishek Paul <jabhishekpaul079@gmail.com>
2025-07-23 14:01:33 +0530

    added new verecel cors

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,7 @@
 export const cookieOptions = {
     httpOnly: true,
     secure: true,
+    secure: process.env.NODE_ENV === 'production',
     sameSite: "None",
     maxAge: 1000 * 60 * 60, // 5 minutes
 }
